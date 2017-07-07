02161c293db9ebc6b2ac7d3af5de7d6fe3735aa7
FaizaJavaid93 <faiza.javaid.12@ucl.ac.uk>
2017-07-07 11:20:12 +0100

    addition of description

--- get_human_ig.py
+++ get_human_ig.py
@@ -1,3 +1,39 @@
+#*************************************************************************
+#
+#   Program:   get the antibody data from a file 
+#   File:      get_human_ig.py
+#
+#   Version:
+#   Date:      07.07.2017
+#   Function:  Read in a file, step through the file one line at a time to extract data (including accession number, organism name, nucleotide sequence and DNA sequence) for gamma heavy chain antibodies. 
+#
+#   Copyright:  (c) UCL, Faiza Javaid, 2017
+#   Author:     Faiza Javaid
+#   Address:    Institute of Structural and Molecular Biology
+#               Division of Biosciences
+#               University College
+#               Gower Street
+#               London
+#               WC1E 6BT
+#   Email:      faiza.javaid.12@ucl.ac.uk
+#
+#*************************************************************************
+#
+#   This program is not in the public domain, but it may be copied
+#   according to the conditions laid out in the accompanying file
+#   COPYING.DOC
+#
+#   The code may be modified as required, but any modifications must be
+#   documented so that the person responsible can be identified. If
+#   someone else breaks this code, I don't want to be blamed for code
+#   that does not work!
+#
+#   The code may not be sold commercially or included as part of a
+#   commercial product except as described in the file COPYING.DOC.
+#
+#*************************************************************************
+
+
 import re
 import json
 import sys
